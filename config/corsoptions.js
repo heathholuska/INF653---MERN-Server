9efaa58623b37f7a2df9fb6b762ceb489b1e3ed0
Heath Holuska <heath.holuska@gmail.com>
2026-05-06 18:21:05 -0500

    Add new allowed origin for CORS

--- config/corsoptions.js
+++ config/corsoptions.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
 	"https://inf653-mern-server.onrender.com",
 	"http://127.0.0.1:5500",
 	"http://localhost:3500",
+	"https://dazzling-snickerdoodle-777101.netlify.app",
 ];
 
 const corsOptions = {
